097a8aacf75d02fc3e9d35833218107c65a9f0e3
zhangyanlei <zhangyanlei189@163.com>
2017-08-29 18:59:42 +0800

    实现中奖列表功能

--- lottey1/First/model/index.js
+++ lottey1/First/model/index.js
@@ -1,9 +1,30 @@
 /**
  * Created by zhangyanlei on 2017/8/25.
  */
-//var conn = require("../db/db");
-//module.exports = conn;
+var conn = require("../db/db");
 
-/*conn.query("select * from country",function(err,result){
-    return result;
-});*/
+function IndexModel(){
+
+}
+
+IndexModel.prototype = {
+    constructor:IndexModel,
+    list:function(fn){
+        var sql = "select u.no,u.name,u.rate,COUNT(r.no) t,SUM(money) money from user u LEFT JOIN record r ON u.no = r.no GROUP BY u.no";
+        conn.query(sql,function(err,result){
+            if(result.length>0){
+                fn({flag:true,data:result});
+            }else{
+                fn({flag:false,data:[]});
+            }
+        });
+    },
+    luck:function(fn){
+
+    }
+}
+
+
+
+
+module.exports = IndexModel;
--- lottey1/First/routes/index.js
+++ lottey1/First/routes/index.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var model = require("../model/index");
+var IndexModel = require("../model/index");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect("/index");
 });
 router.get('/index', function(req, res, next) {
-  res.render('index', { title: '抽奖' });
-});
-router.get('/user', function(req, res, next) {
-  res.redirect("/user/user");
+  var indexModel = new IndexModel();
+  indexModel.list(function(ret){
+    console.log(ret);
+    console.log(222222);
+    Promise.all([ret]).then(function(){//由于nodejs的异步操作，所以使用延迟渲染
+      if(ret.flag){
+        console.log(ret);
+        console.log("****************55555555555555");
+        res.render('index', { title: '抽奖',data:ret.data});
+      }else{
+        res.render('index', { title: '抽奖',data:[]});
+      }
+    });
+
+  });
+
 });
 module.exports = router;
